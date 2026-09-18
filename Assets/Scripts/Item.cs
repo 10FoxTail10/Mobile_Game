d324ee46d64aa539ed47f4912fd3f4d0f8e57002
Рыжаков Павел <10foxtail10@gmail.com>
2026-09-18 13:48:46 +0300

    Revert "Весь проект на ГитХабе"
    This reverts commit d036f3d48dd35022bf7923e312ee31ac2bbacb1e.

--- Assets/Scripts/Item.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public class Item : MonoBehaviour
-{
-    [SerializeField] private ItemData itemData;
-
-    public ItemData Data => itemData;
-}
--- Assets/Scripts/ItemData.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "NewItem", menuName = "Items/Item Data")]
-public class ItemData : ScriptableObject
-{
-    public string itemName;
-    public Sprite icon;
-}
